eb69ac121179c09ed90ccda6aea9345e94aba257
Muhammad Reza Maulana <43521076+MuhammadRezaMaulana@users.noreply.github.com>
2018-09-30 17:39:23 +0700

    Update formawal.php
    Muhammad Reza Maulana_6701174021_D3MI - 41 - 04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,25 @@
-<?php
+<?php 
+	
+	//include 'hobi.php';
 
+ ?>
 
+<h2><center>Login</center></h2>
+<form action="hobi.php" method="GET">
 
-?>
+<table align="center">
+	<tr>
+		<td>ID :</td>  
+		<td><input type="text" name="id"></td>
+	</tr>
+	<tr>	
+		<td>Password :</td> 
+		<td><input type="password" name="password"></td>
+	</tr>
+	<tr>	
+		<td><input type="submit" name="submit" value="kirim"></td>
+	</tr>
+
+</table>
+	
+</form>
